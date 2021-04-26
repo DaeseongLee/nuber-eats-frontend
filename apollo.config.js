657810aa1c578642b22d137c4175f0e92d13d6aa
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-26 13:31:49 +0900

    Add Apollo Codegen

---
+++ apollo.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: {
+        inCludes: ["./src/**/*.tsx"],
+        tagName: 'gql',
+        service: {
+            name: "nuber-eats-backend",
+            url: "http://localhost:4000/graphql",
+        },
+    },
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,10 +2,11 @@ import { gql, useMutation, } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
+import { loginMutation, loginMutationVariables } from "../__generated__/loginMutation";
 
 
 const LOGIN_MUTATION = gql`
-    mutation PotatoMutation($email: String, $password: String!) {
+    mutation loginMutation($email: String, $password: String!) {
         login(input: {email:$email, password: $password}){
             ok
             token
@@ -21,7 +22,7 @@ interface ILoginForm {
 
 export const Login = () => {
     const { register, getValues, formState: { errors }, handleSubmit } = useForm<ILoginForm>();
-    const [loginMutation, { loading, error, data }] = useMutation(LOGIN_MUTATION);
+    const [loginMutation, { data }] = useMutation<loginMutation, loginMutationVariables>(LOGIN_MUTATION);
 
     const onSubmit = () => {
         const { email, password } = getValues();
