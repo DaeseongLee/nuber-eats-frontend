b41864e3324eb839419f056a33456d643819b9b1
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-26 18:01:20 +0900

    Add httpLink to send the token to the server

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,4 +1,5 @@
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache, makeVar } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 import { LOCALSTORAGE_TOKEN } from './constant';
 
 
@@ -6,11 +7,21 @@ const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(false);
 export const authTokenVar = makeVar(token);
 
-console.log("default value of isLoggedInVar is:", isLoggedInVar());
-console.log("default value of authTokenVar is:", authTokenVar());
+const httpLink = createHttpLink({
+    uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            "x-jwt": authTokenVar() || "",
+        }
+    }
+})
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache({
         typePolicies: {
             Query: {
