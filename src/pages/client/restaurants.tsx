eab18bf436eafb1eb2d7c165b6e50c5f6e5c7aba
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-27 19:21:55 +0900

    Add Paging

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Restaurant } from '../../components/restaurant';
 import { restaurantsPageQuery, restaurantsPageQueryVariables } from '../../__generated__/restaurantsPageQuery';
 
 const RESTAURANT_QUERY = gql`
@@ -35,14 +36,16 @@ const RESTAURANT_QUERY = gql`
 `
 
 const Restaurants = () => {
+    const [page, setPage] = useState(1);
     const { data, loading } = useQuery<restaurantsPageQuery, restaurantsPageQueryVariables>(RESTAURANT_QUERY, {
         variables: {
             input: {
-                page: 1,
+                page,
             },
         },
     });
-    console.log(data);
+    const onNextPageClick = useCallback(() => setPage(current => current + 1), []);
+    const onPrevPageClick = useCallback(() => setPage(current => current - 1), []);
     return (
         <div>
             <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
@@ -53,7 +56,7 @@ const Restaurants = () => {
                 />
             </form>
             {!loading && (
-                <div className="max-w-screen-2xl mx-auto mt-8">
+                <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
                     <div className="flex justify-around max-w-sm mx-auto">
                         {data?.allCategories.categories?.map(category => (
                             <div className="flex flex-col group items-center cursor-pointer">
@@ -65,17 +68,36 @@ const Restaurants = () => {
                             </div>
                         ))}
                     </div>
-                    <div className="grid mt-10 grid-cols-3 gap-x-5 gap-y-10">
+                    <div className="grid mt-16 grid-cols-3 gap-x-5 gap-y-10">
                         {data?.restaurants.results?.map(restaurant => (
-                            <div>
-                                <div style={{ backgroundImage: `url(${restaurant.coverImg})` }}
-                                    className="bg-red-500 bg-cover bg-center mb-3 py-28"></div>
-                                <h3 className="text-xl font-medium">{restaurant.name}</h3>
-                                <span className="border-t-2 border-gray-200">
-                                    {restaurant.category?.name}
-                                </span>
-                            </div>
+                            <Restaurant
+                                id={restaurant.id + ""}
+                                coverImg={restaurant.coverImg}
+                                name={restaurant.name}
+                                categoryName={restaurant.category?.name}
+                            />
                         ))}
+                    </div>
+                    <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+                        {page > 1 ? (
+                            <button
+                                onClick={onPrevPageClick}
+                                className="focus:outline-none font-medium text-2xl"
+                            >
+                                &larr;
+                            </button>
+                        ) : (<div></div>)}
+                        <span>
+                            Page {page} of {data?.restaurants.totalPages}
+                        </span>
+                        {page !== data?.restaurants.totalPages ? (
+                            <button
+                                onClick={onNextPageClick}
+                                className="focus:outline-none font-medium text-2xl"
+                            >
+                                &rarr;
+                            </button>
+                        ) : (<div></div>)}
                     </div>
                 </div>
             )}
