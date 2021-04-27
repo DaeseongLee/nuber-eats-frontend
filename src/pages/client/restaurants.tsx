309d546d9fe0611e09b178843a68f72d1926ac0c
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-04-27 22:52:05 +0900

    Add Category

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -3,8 +3,9 @@ import React, { useCallback, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
 import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import { Restaurant } from '../../components/restaurant';
-import { RESTAURANT_FRAGMENT } from '../../fragments';
+import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from '../../fragments';
 import { restaurantsPageQuery, restaurantsPageQueryVariables } from '../../__generated__/restaurantsPageQuery';
 
 const RESTAURANT_QUERY = gql`
@@ -13,11 +14,7 @@ const RESTAURANT_QUERY = gql`
             ok
             error
             categories {
-                id
-                name
-                coverImg
-                slug
-                restaurantCount
+              ...CategoryParts
             }
         }
         restaurants(input: $input) {
@@ -31,6 +28,7 @@ const RESTAURANT_QUERY = gql`
         }
     }
     ${RESTAURANT_FRAGMENT}
+    ${CATEGORY_FRAGMENT}
 `
 interface IFormProps {
     searchTerm: string;
@@ -75,13 +73,15 @@ const Restaurants = () => {
                 <div className="max-w-screen-2xl pb-20 mx-auto mt-8">
                     <div className="flex justify-around max-w-sm mx-auto">
                         {data?.allCategories.categories?.map(category => (
-                            <div className="flex flex-col group items-center cursor-pointer" key={category.id}>
-                                <div className="w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
-                                    style={{ backgroundImage: `url(${category.coverImg})` }}></div>
-                                <span className="mt-1 text-sm text-center font-medium">
-                                    {category.name}
-                                </span>
-                            </div>
+                            <Link key={category.id} to={`/category/${category.slug}`} >
+                                <div className="flex flex-col group items-center cursor-pointer" >
+                                    <div className="w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full"
+                                        style={{ backgroundImage: `url(${category.coverImg})` }}></div>
+                                    <span className="mt-1 text-sm text-center font-medium">
+                                        {category.name}
+                                    </span>
+                                </div>
+                            </Link>
                         ))}
                     </div>
                     <div className="grid mt-16 lg:grid-cols-3 gap-x-5 gap-y-10">
