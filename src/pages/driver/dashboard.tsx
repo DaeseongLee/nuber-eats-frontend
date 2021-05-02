47433bc3edf463b1600d28bcb591d8255fc23588
DaeseongLee <47413926+DaeseongLee@users.noreply.github.com>
2021-05-02 20:52:15 +0900

    Add Painting Directions

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -6,10 +6,17 @@ interface ICoords {
     lng: number;
 }
 
+interface IDriverProps {
+    lat: number;
+    lng: number;
+    $hover?: any;
+}
+const Driver: React.FC<IDriverProps> = () => <div className="text-lg">🚖</div>
+
 export const Dashboard = () => {
     const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
     const [map, setMap] = useState<any>();
-    const [maps, setMaps] = useState<any>();
+    const [maps, setMaps] = useState<google.maps.Map>(); //google은 window안에 있는 내장객체이다
 
     //@ts-ignore
     const onSuccess = ({ coords: { latitude, longitude } }: Position) => {
@@ -20,9 +27,18 @@ export const Dashboard = () => {
         console.log(error);
     }
     useEffect(() => {
-        if (map && maps) {
-            map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
-        }
+        // if (map && maps) {
+        //     map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
+        //     const geocoder = new google.maps.Geocoder();
+        //     geocoder.geocode(
+        //         {
+        //             location: new google.maps.LatLng(driverCoords.lat, driverCoords.lng),
+        //         },
+        //         (results, status) => {
+        //             console.log(status, results);
+        //         }
+        //     )
+        // }
     }, [driverCoords.lat, driverCoords.lng]);
 
     useEffect(() => {
@@ -32,11 +48,41 @@ export const Dashboard = () => {
     }, []);
 
     const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
-        map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+        map.panTo(new google.maps.LatLng(driverCoords.lat, driverCoords.lng));
         setMap(map);
         setMaps(maps);
     };
 
+    const onGetRouteClick = () => {
+        if (map) {
+            const directionsService = new google.maps.DirectionsService();
+            const directionsRender = new google.maps.DirectionsRenderer();
+            console.log('directionsService', directionsService);
+            console.log('directionsRender', directionsRender);
+
+            directionsRender.setMap(map);
+            directionsService.route(
+                {
+                    origin: {
+                        location: new google.maps.LatLng(
+                            driverCoords.lat,
+                            driverCoords.lng
+                        )
+                    },
+                    destination: {
+                        location: new google.maps.LatLng(
+                            driverCoords.lat + 0.05,
+                            driverCoords.lng + 0.05
+                        ),
+                    },
+                    travelMode: google.maps.TravelMode.DRIVING,
+
+                }, (result) => {
+                    directionsRender.setDirections(result);
+                })
+
+        }
+    }
     return (
         <div>
             <div className="overflow-hidden"
@@ -44,7 +90,7 @@ export const Dashboard = () => {
             >
                 <GoogleMapReact
                     defaultZoom={16}
-                    draggable={false}
+                    draggable={true}
                     onGoogleApiLoaded={onApiLoaded}
                     defaultCenter={{
                         lat: 36.1488339,
@@ -52,17 +98,10 @@ export const Dashboard = () => {
                     }}
                     bootstrapURLKeys={{ key: "AIzaSyDkRvqqO0XomHAIZyeJcb5NX9W8sZWQ-KE" }}
                 >
-                    <div
-                        //@ts-ignore
-                        lat={driverCoords.lat}
-                        lng={driverCoords.lng}
-                        className="text-lg">
-                        🚴‍♂️
-                        </div>
-
-
+                    <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
                 </GoogleMapReact>
             </div>
+            <button onClick={onGetRouteClick}>Get route</button>
         </div>
     )
 }
